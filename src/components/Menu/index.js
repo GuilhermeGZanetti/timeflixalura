e24df8c240b2947f7bf4eb3189fa6c6250b5db8c
Guilherme Goes Zanetti <guigoes8@hotmail.com>
2020-07-28 22:18:20 -0300

    Aula 2 - Parte 1
    Melhora do Carrosel,
    React Router DOM - Não precisar atualizar a página

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import Logo from '../../assets/img/logo.png';
 import "./Menu.css";
 //import {MenuBase} from './styles'; //Não consegui fazer o menu com o styled-components
@@ -8,11 +9,11 @@ import Button from '../Button';
 function Menu(){
     return(
         <div className="Menu"> 
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="Timeflix Logo"/>
-            </a>
+            </Link>
 
-            <Button as='a' className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Guarde um vídeo no tempo
             </Button>
         </div>
